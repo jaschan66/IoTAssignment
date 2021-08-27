2df88874f7676d1c18b36a0864aad0161515ba70
jaschan66 <57925035+jaschan66@users.noreply.github.com>
2021-08-27 22:00:33 +0800

    testing some simple read and write between mobile and firebase

--- app/src/main/java/my/com/iotassignment/MainActivity.kt
+++ app/src/main/java/my/com/iotassignment/MainActivity.kt
@@ -2,10 +2,39 @@ package my.com.iotassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import my.com.iotassignment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val database = Firebase.database
+    val ref = database.getReference("smartSecurity")
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSend.setOnClickListener{ sendToFirebasePls() }
+
+
+
     }
+
+    private fun sendToFirebasePls() {
+        var text = binding.edtName.text.toString().trim().uppercase()
+
+        ref.child("tripWire")
+            .setValue(text)
+
+        ref.child("tripWire")
+            .get()
+            .addOnSuccessListener {
+                binding.txtTest.text = it.value.toString()
+            }
+    }
+
+
 }
